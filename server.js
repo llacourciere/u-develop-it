0e7b998e76b1f16c761b3a87165513e5cb62f12d
LaLacou <108777796+users.noreply.github.com>
2022-09-26 18:56:55 -0700

    added to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,7 +7,18 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 //default response for any other request (Not Found)
 app.use((req,res)=> {
     res.status(404).end();
